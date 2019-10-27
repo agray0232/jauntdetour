f3de8e3b63c1f2f0e26e5fe881dc11c9da7539b5
Anthony Gray <agray0232@gmail.com>
2019-10-27 01:51:42 -0400

    Decoded the polyline strings returned with routes

--- backend/app/modules/routeAPI.js
+++ backend/app/modules/routeAPI.js
@@ -9,6 +9,11 @@ var config = require("../../config/config.js");
 const axios = require("axios");
 
 /**
+ * Import polyline encoding utlity
+ */
+const polylineEncoder = require("polyline-encoded");
+
+/**
  * Expose the functions that we need in other files
  */
 module.exports = {
@@ -23,7 +28,8 @@ module.exports = {
       axios
         .get(url)
         .then(response => {
-          resolve(response.data);
+          var decodedData = decodePolylines(response.data);
+          resolve(decodedData);
         })
         .catch(error => {
           console.log("error");
@@ -35,3 +41,36 @@ module.exports = {
     return promise;
   }
 };
+
+/**
+ * Decodes all of the encoded polyline strings in the routes
+ *
+ * @param data - Raw data containing encoded polyline strings
+ * @return Data containing decoded lat/lon arrays representing the encoded polyline strings
+ */
+function decodePolylines(data) {
+  // Extract the routes
+  var decodedData = {
+    routes: data.routes
+  };
+
+  // Format each route
+  decodedData.routes.forEach(function(route) {
+    // Format each leg
+    route.legs.forEach(function(leg) {
+      // Format each step
+      leg.steps.forEach(function(step) {
+        // Decode the encoded polyline point string
+        step.polyline.decodedPoints = polylineEncoder.decode(
+          step.polyline.points
+        );
+      });
+    });
+    // Decode the encoded overview polyline point string
+    route.overview_polyline.decodedPoints = polylineEncoder.decode(
+      route.overview_polyline.points
+    );
+  });
+
+  return decodedData;
+}
