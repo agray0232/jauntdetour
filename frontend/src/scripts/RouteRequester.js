fef881c2ad090cd68a3c4d729662e5120d111e07
Anthony Gray <agray0232@gmail.com>
2019-11-09 23:50:11 -0500

    Modified for dev env

--- frontend/src/scripts/RouteRequester.js
+++ frontend/src/scripts/RouteRequester.js
@@ -27,7 +27,7 @@ export default class RouteRequester {
     var promise = new Promise(function(resolve, reject) {
       // Send an axios GET request to the server
       axios
-        .get("https://www.jauntdetour.com/backend/route", {
+        .get("http://localhost:8080/route", {
           headers: {
             "Content-Type": "application/json"
           },
