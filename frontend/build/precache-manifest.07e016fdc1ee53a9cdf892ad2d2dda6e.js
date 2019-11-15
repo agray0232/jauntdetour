564a410684d9b37e999b5ed841446034795393a9
Anthony Gray <agray0232@gmail.com>
2019-11-15 15:38:28 -0500

    Added build

---
+++ frontend/build/precache-manifest.07e016fdc1ee53a9cdf892ad2d2dda6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e924bdaac989331c67df68c26b7bcce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d9acf05089de78a069",
+    "url": "/static/css/main.27ef00de.chunk.css"
+  },
+  {
+    "revision": "397eb493822002839895",
+    "url": "/static/js/2.5a924ac1.chunk.js"
+  },
+  {
+    "revision": "d3d9acf05089de78a069",
+    "url": "/static/js/main.ba31dd01.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  }
+]);
